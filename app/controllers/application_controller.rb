167b091fde0dd35d3b2fc75e404a74fab480ce49
kubo shizuma <forothers.295@gmail.com>
2016-10-09 09:08:04 +0900

    Update nav for plan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :weekly_plan_status
+
+  def weekly_plan_status
+    @is_next_week_plan = WeeklyPlan.exists?(user_id: current_user.id, start_date: Date.today.beginning_of_week + 1.week)
+    @is_this_week_plan = WeeklyPlan.exists?(user_id: current_user.id, start_date: Date.today.beginning_of_week)
+  end
 end
